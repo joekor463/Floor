6eed8aefeab99f2b901e998e69f3cc2f7724bf0a
joekor463 <63947827+joekor463@users.noreply.github.com>
2020-05-08 07:46:55 +0800

    Add files via upload

---
+++ Carpet.java
@@ -0,0 +1,13 @@
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        if (cost <= 0) cost = 0;
+        else this.cost = cost;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+
+}
